9df73715fcfab615cdde3be12b7541d2d49a2d2e
Simran Soni <simransoni2307@gmail.com>
2026-03-29 01:58:16 +0530

    Add files via upload

---
+++ Problem_632.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem_632 {
+    public static void main(String[] args) {
+        List<List<Integer>> nums = new ArrayList<>();
+        nums.add(Arrays.asList(4,10,15,24,26));
+        nums.add(Arrays.asList(0,9,12,20));
+        nums.add(Arrays.asList(8,15,22,30));
+        System.out.println(nums);
+    }
+}
